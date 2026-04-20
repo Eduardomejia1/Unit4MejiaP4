1066968d45ad1cc1e8c0e5a2e3e1b488eca39bb9
Eduardomejia1 <730845@student.dusd.net>
2026-04-20 12:02:57 -0700

    Created Spawn Manager
    Created spawn Manager script.

--- P4MejiaUnit4/Assets/Scripts/Enemy.cs
+++ P4MejiaUnit4/Assets/Scripts/Enemy.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -16,6 +18,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyRb.AddForce((player.transform.position - transform.position) * speed);
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
     }
 }
